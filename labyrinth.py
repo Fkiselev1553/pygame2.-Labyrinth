70ab7b721a8312e9085c0d776c1dde9c4ddc4384
Fkiselev1553 <118972763+Fkiselev1553@users.noreply.github.com>
2022-12-26 14:45:47 +0300

    Update labyrinth.py
    Теперь программа работает, но нет клеточек

--- labyrinth.py
+++ labyrinth.py
@@ -9,20 +9,19 @@ BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 #kletka
 cell = 80
-grid_width = WIDTH / cell
-grid_height = HEIGHT / cell
+grid_width = WIDTH // cell
+grid_height = HEIGHT // cell
 #background
 screen.fill(WHITE)
 for i in range(grid_width):
-	for f in range(grid_height):
-		#rectangle
-		#rectangle = pygame.rect(i*cell, j*cell, cell, cell
-		pygame.draw.rect(screen,WHITE,pygame.rect(i*cell, j*cell, cell,cell), 1)
+        for j in range(grid_height):
+                #rectangle = pygame.rect(i*cell, f*cell, cell, cell)
+                pygame.draw.rect(screen,BLACK,pygame.Rect(i*cell, j*cell, cell, cell), 1)
 pygame.display.flip
 running = True
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.quit:
-			running = False
-			break
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        running = False
+                        break
 pygame.quit()
